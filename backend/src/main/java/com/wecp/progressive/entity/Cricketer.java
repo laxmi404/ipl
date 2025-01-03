8b46f4e9e9ef16ef5cc95c43ce118dfe74889832
username <wecp-dummy@gmail.com>
2025-01-03 13:33:15 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/entity/Cricketer.java
+++ backend/src/main/java/com/wecp/progressive/entity/Cricketer.java
@@ -1,7 +1,16 @@
 package com.wecp.progressive.entity;
- 
-public class Cricketer {
-    private int CricketerId;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Comparator;
+
+@Entity
+public class Cricketer implements Comparable<Cricketer> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cricketerId;
     private int teamId;
     private String cricketerName;
     private int age;
@@ -10,16 +19,12 @@ public class Cricketer {
     private String role;
     private int totalRuns;
     private int totalWickets;
+
     public Cricketer() {
-       
-       
-       
     }
-   
-   
-    public Cricketer(int cricketerId, int teamId, String cricketerName, int age, String nationality, int experience,
-            String role, int totalRuns, int totalWickets) {
-        CricketerId = cricketerId;
+
+    public Cricketer(int cricketerId, int teamId, String cricketerName, int age, String nationality, int experience, String role, int totalRuns, int totalWickets) {
+        this.cricketerId = cricketerId;
         this.teamId = teamId;
         this.cricketerName = cricketerName;
         this.age = age;
@@ -29,66 +34,82 @@ public class Cricketer {
         this.totalRuns = totalRuns;
         this.totalWickets = totalWickets;
     }
- 
- 
+
     public int getCricketerId() {
-        return CricketerId;
+        return cricketerId;
     }
+
     public void setCricketerId(int cricketerId) {
-        CricketerId = cricketerId;
+        this.cricketerId = cricketerId;
     }
+
     public int getTeamId() {
         return teamId;
     }
+
     public void setTeamId(int teamId) {
         this.teamId = teamId;
     }
+
     public String getCricketerName() {
         return cricketerName;
     }
+
     public void setCricketerName(String cricketerName) {
         this.cricketerName = cricketerName;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public String getNationality() {
         return nationality;
     }
+
     public void setNationality(String nationality) {
         this.nationality = nationality;
     }
+
     public int getExperience() {
         return experience;
     }
+
     public void setExperience(int experience) {
         this.experience = experience;
     }
+
     public String getRole() {
         return role;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
+
     public int getTotalRuns() {
         return totalRuns;
     }
+
     public void setTotalRuns(int totalRuns) {
         this.totalRuns = totalRuns;
     }
+
     public int getTotalWickets() {
         return totalWickets;
     }
+
     public void setTotalWickets(int totalWickets) {
         this.totalWickets = totalWickets;
     }
- 
-   
-   
- 
- 
- 
+
+    @Override
+    public int compareTo(Cricketer otherCricketer) {
+        return Comparator.comparingInt(Cricketer::getExperience)
+                .compare(this, otherCricketer);
+    }
 }
